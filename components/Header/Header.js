5a7638df64832f3785013dc5d6fcbec171d373ad
Bruneljohnson <photography@bruneljohnson.com>
2022-02-05 22:51:36 +0000

    fixing logo

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import classes from "./Header.module.css";
-import Image from "next/image";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
 import {
